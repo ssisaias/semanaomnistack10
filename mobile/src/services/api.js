5dbe14e206de448ff3e4f7300eb99f101b62e4ac
Isaias S. Silva <luiz.isaias95@gmail.com>
2020-01-19 11:35:02 -0300

    Mobile code

---
+++ mobile/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+    // Change this to match the host on EXPO (and the port on the backend)
+    baseURL: 'http://192.168.0.105:3003'
+})
+
+export default api;
